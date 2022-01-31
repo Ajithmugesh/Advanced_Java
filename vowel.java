9844a4fd445f13f7152bf79ac01c3622bffde69a
Ajithmugesh <98526693+Ajithmugesh@users.noreply.github.com>
2022-01-31 14:17:41 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,13 @@
+package charac;
+
+public class vowel {
+	public static void main(String[] args)
+	{
+		char ch = 'q';
+		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+		System.out.println(ch + " is vowel");
+		else
+		System.out.println(ch + " is consonant");
+	}
+
+}
