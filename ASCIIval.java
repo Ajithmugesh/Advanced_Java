a4ae3ce15cf09835e51b290f586a5775b375e6dd
Ajithmugesh <98526693+Ajithmugesh@users.noreply.github.com>
2022-01-31 14:22:30 +0530

    Add files via upload

---
+++ ASCIIval.java
@@ -0,0 +1,14 @@
+package findASCII;
+
+public class ASCIIval {
+	public static void main(String[] args)
+	{
+		char ch = 'H';
+		int ascii = ch;
+		int castAscii = (int) ch;
+		System.out.println("The ASCII value of " + ch + " is: " + ascii);
+		System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+		
+	}
+
+}
