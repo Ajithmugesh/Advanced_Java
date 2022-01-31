47b49816ad45530dcf47a2720f36df2145595720
Ajithmugesh <98526693+Ajithmugesh@users.noreply.github.com>
2022-01-31 15:28:16 +0530

    Add files via upload

---
+++ typeca.java
@@ -0,0 +1,15 @@
+package typecaast;
+
+public class typeca {
+	public static void main(String[] args) 
+	{
+		byte b=10;
+		int i=b;
+		
+		byte d=(byte)i;
+		byte x=10;
+		byte y=20;
+		int sum=x*y;
+		System.out.println(sum);
+     }
+}
