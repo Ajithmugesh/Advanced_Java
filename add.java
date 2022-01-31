a19d6a2476437cd94914aeafac8e9ee2551f4a42
Ajithmugesh <98526693+Ajithmugesh@users.noreply.github.com>
2022-01-31 14:10:07 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+package addition;
+
+public class add {
+ public static void main(String[] args) {
+	System.out.println("Enter any two numbers:");
+	int first = 100;
+	int second = 200;
+	System.out.println(first+","+second);
+	int sum = first + second;
+	System.out.println("the Sum is"+sum);
+}
+}
