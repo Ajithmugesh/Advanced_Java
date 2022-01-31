91fd511182ef9daa9fe580dfb9127d211c481f4a
Ajithmugesh <98526693+Ajithmugesh@users.noreply.github.com>
2022-01-31 14:23:37 +0530

    Add files via upload

---
+++ largestnum.java
@@ -0,0 +1,15 @@
+package findlargest;
+
+public class largestnum {
+public static void main(String[] args) 
+{
+	double a = 2.5, b = 6.7, c =4.3;
+	if( a >= b && a >= c)
+	System.out.println(a + " is the largest number.");
+	else if (b >= a && b >= c)
+	System.out.println(b + " is the largest number.");
+	else
+	System.out.println(c+ " is the largest number.");
+	
+}
+}
